bc33bf08f9967a4679762367254bc7e4f6d6cc0d
Somrak <genminigpt@gmail.com>
2026-07-09 20:32:02 +0700

    Add basePath and assetPrefix for gh-pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  basePath: '/sahawat.github.io',
+  assetPrefix: '/sahawat.github.io/',
 };
 
 module.exports = nextConfig;
