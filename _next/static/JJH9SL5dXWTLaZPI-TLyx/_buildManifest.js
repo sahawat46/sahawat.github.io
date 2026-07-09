dec681c9ee4074684d7812f863ad1122d682e8f0
Somrak <genminigpt@gmail.com>
2026-07-09 20:31:46 +0700

    Updates

--- _next/static/JJH9SL5dXWTLaZPI-TLyx/_buildManifest.js
+++ _next/static/JJH9SL5dXWTLaZPI-TLyx/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sahawat.github.io//_next/:path+",
+        "destination": "/sahawat.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
